58506aace300c47a49dac9e14976fb31621eff5e
MeliiZa <melissazandman@icloud.com>
2019-06-13 20:55:51 -0700

    work image

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,12 +1,14 @@
 class Homepage extends React.Component {
-	render(){
-		return 
-        <div>
-            <p>"Welcome to the homepage."</p> 
-            <a href="/cards"> CARDS HERE </a>
-            <img src= "/static/img/ballonicorn.jpg"> ;
-         </div>
+	render() {
+		return ( 
+            <div className="greeting">
+                <p>Welcome to the homepage.</p> 
+                <a href="/cards"> CARDS HERE </a>
+                <img src="static/img/seedpy.jpg"/>
+             </div>
+        );
 	}
 }
+
 
 ReactDOM.render(<Homepage />, document.getElementById('app'));
